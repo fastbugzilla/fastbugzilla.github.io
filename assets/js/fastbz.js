3b7f848c400c35ab873576b78940ada35f86c97a
Pranav Maddi <pranav.maddi@gmail.com>
2014-07-31 17:38:58 -0700

    css

--- assets/js/fastbz.js
+++ assets/js/fastbz.js
@@ -1,30 +1,33 @@
 $(document).ready(function() {
-    var qb_query = '{"from":"public_bugs","select":["bug_id"]}';
-    qb_query = $.parseJSON( qb_query );
+    if (location.search != '') {
+        var qb_query = '{"from":"public_bugs","select":["bug_id"]}';
+        qb_query = $.parseJSON( qb_query );
 
-    var qbString = bzSearchToQb( location.href, (new Date).getTime(), (new Date).getTime(), 'public_bugs' );
-    qb_query["esfilter"] = $.parseJSON(qbString)['esfilter'];
+        var qbString = bzSearchToQb( location.href, (new Date).getTime(), (new Date).getTime(), 'public_bugs' );
+        qb_query["esfilter"] = $.parseJSON(qbString)['esfilter'];
 
-    qb_query["esfilter"] = {"and": [{"range":{"expires_on":{"gte":(new Date).getTime()}}},qb_query["esfilter"] ]}
-    console.log( qb_query );
+        qb_query["esfilter"] = {"and": [{"range":{"expires_on":{"gte":(new Date).getTime()}}},qb_query["esfilter"] ]}
+        console.log( qb_query );
 
-    ESQueryRunner( 
-        qb_query, 
-        function( response ){ // Executes after data is returned from ES.
-            // Format the returned ElasticSearch data for Rickshaw compatibility
+        ESQueryRunner( 
+            qb_query, 
+            function( response ){ // Executes after data is returned from ES.
+                // Format the returned ElasticSearch data for Rickshaw compatibility
 
-            buglist = [];
+                buglist = [];
 
-            jQuery.each(response.list, function(i, id) {
-            	buglist.push(id["bug_id"]);
-            });
-            console.log(buglist);
+                jQuery.each(response.list, function(i, id) {
+                    buglist.push(id["bug_id"]);
+                });
+                console.log(buglist);
 
-            var newURL = URI("https://bugzilla.mozilla.org/buglist.cgi").search({"bug_id": buglist});
-            console.log(newURL.toString());
+                var newURL = URI("https://bugzilla.mozilla.org/buglist.cgi").search({"bug_id": buglist});
+                console.log(newURL.toString());
 
-            window.location.href = newURL.toString();
-        }
-    );
+                window.location.href = newURL.toString();
+            }
+        );
+    };
+    
 })
 
