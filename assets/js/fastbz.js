79b4b35c90119fbd4cf2ef029beebc14cf1f22b0
Pranav Maddi <pranav.maddi@gmail.com>
2014-07-31 17:53:07 -0700

    css

--- assets/js/fastbz.js
+++ assets/js/fastbz.js
@@ -1,33 +1,42 @@
+function loadURL (bzURL) {
+    var qb_query = '{"from":"public_bugs","select":["bug_id"]}';
+    qb_query = $.parseJSON( qb_query );
+
+    var qbString = bzSearchToQb( bzURL, (new Date).getTime(), (new Date).getTime(), 'public_bugs' );
+    qb_query["esfilter"] = $.parseJSON(qbString)['esfilter'];
+
+    qb_query["esfilter"] = {"and": [{"range":{"expires_on":{"gte":(new Date).getTime()}}},qb_query["esfilter"] ]}
+    console.log( qb_query );
+
+    ESQueryRunner( 
+        qb_query, 
+        function( response ){ // Executes after data is returned from ES.
+            // Format the returned ElasticSearch data for Rickshaw compatibility
+
+            buglist = [];
+
+            jQuery.each(response.list, function(i, id) {
+                buglist.push(id["bug_id"]);
+            });
+            console.log(buglist);
+
+            var newURL = URI("https://bugzilla.mozilla.org/buglist.cgi").search({"bug_id": buglist});
+            console.log(newURL.toString());
+
+            window.location.href = newURL.toString();
+        }
+    );
+}
+
+
 $(document).ready(function() {
     if (location.search !== '') {
-        var qb_query = '{"from":"public_bugs","select":["bug_id"]}';
-        qb_query = $.parseJSON( qb_query );
-
-        var qbString = bzSearchToQb( location.href, (new Date).getTime(), (new Date).getTime(), 'public_bugs' );
-        qb_query["esfilter"] = $.parseJSON(qbString)['esfilter'];
-
-        qb_query["esfilter"] = {"and": [{"range":{"expires_on":{"gte":(new Date).getTime()}}},qb_query["esfilter"] ]}
-        console.log( qb_query );
-
-        ESQueryRunner( 
-            qb_query, 
-            function( response ){ // Executes after data is returned from ES.
-                // Format the returned ElasticSearch data for Rickshaw compatibility
-
-                buglist = [];
-
-                jQuery.each(response.list, function(i, id) {
-                    buglist.push(id["bug_id"]);
-                });
-                console.log(buglist);
-
-                var newURL = URI("https://bugzilla.mozilla.org/buglist.cgi").search({"bug_id": buglist});
-                console.log(newURL.toString());
-
-                window.location.href = newURL.toString();
-            }
-        );
-    };
+        $( "#find" ).click(function () {
+            loadURL( $(this).val() );
+        })  
+    } else {
+        loadURL( location.href );
+    }
     
 })
 
